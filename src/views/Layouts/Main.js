4034330d585740ef38ef0eee7c3c9b8ebe961328
Dan Kyung <dan@dank.tw>
2018-03-17 18:36:11 -0400

    Responsive

--- src/views/Layouts/Main.js
+++ src/views/Layouts/Main.js
@@ -5,8 +5,8 @@ import './Main.scss';
 const Main = props => ({
   render() {
     return (
-      <div className="ui container grid centered">
-        <div className="seven wide column">
+      <div className="ui stackable container grid centered">
+        <div className="ten wide tablet seven wide computer column">
           <div className="ui piled very padded text segment">
             {props.children}
           </div>
--- src/views/Screens/Home.js
+++ src/views/Screens/Home.js
@@ -17,20 +17,23 @@ class Home extends Component {
           <h2 className="my-0 mx-0 mb-1">Dan Kyung</h2>
           <h4 className="text-muted my-0 mx-0 mb-2">Software Developer</h4>
           <div className="ui grid centered">
-            <div className="two wide column center aligned">
-              <Social color="#007BB5" icon="linkedin in" href="https://www.linkedin.com/in/dannk"/>
-            </div>
-            <div className="two wide column center aligned">
-              <Social color="#24292E" icon="github" href="https://github.com/dank"/>
-            </div>
-            <div className="two wide column center aligned">
-              <Social color="#C32AA3" icon="instagram" href="https://www.instagram.com/dan.kyung/"/>
-            </div>
-            <div className="two wide column center aligned">
               <Social color="#1DA1F2" icon="twitter" href="https://twitter.com/danktw"/>
-            </div>
-            <div className="two wide column center aligned">
-              <Social color="#AAAAAA" icon="envelope" href="mailto:dan@kyung.ca"/>
+            <div className="doubling five column row">
+              <div className="two wide column center aligned">
+                <Social color="#007BB5" icon="linkedin in" href="https://www.linkedin.com/in/dannk" />
+              </div>
+              <div className="two wide column center aligned">
+                <Social color="#24292E" icon="github" href="https://github.com/dank" />
+              </div>
+              <div className="two wide column center aligned">
+                <Social color="#C32AA3" icon="instagram" href="https://www.instagram.com/dan.kyung/" />
+              </div>
+              <div className="two wide column center aligned">
+                <Social color="#1DA1F2" icon="twitter" href="https://twitter.com/danktw" />
+              </div>
+              <div className="two wide column center aligned">
+                <Social color="#AAAAAA" icon="envelope" href="mailto:dan@kyung.ca" />
+              </div>
             </div>
           </div>
         </div>
